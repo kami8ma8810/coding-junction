9eeb0ae616378b69e33e1b48b8a8add4ca911f01
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2021-09-05 17:52:03 +0900

    vueファイルテンプレ作成中

--- pages/index.vue
+++ pages/index.vue
@@ -4,29 +4,37 @@
     <!-- Page Header -->
     <header class="header">
       <div class="header-inner">
-      <div class="header-logo">
-        <img src="~/assets/images/logo-main.svg" alt="" width="50" height="auto">
-      </div>
-      <h1 class="header-title">Coding Junction</h1>
+        <div class="header-logo">
+          <img
+            src="~/assets/images/logo-main.svg"
+            alt=""
+            width="50"
+            height="auto"
+          />
+        </div>
+        <h1 class="header-title">Coding Junction</h1>
       </div>
     </header>
 
     <!-- Main Content -->
-    <main>
-      メインコンテンツ
-    </main>
+    <main>メインコンテンツ</main>
   </div>
 </template>
 
 <script>
-  export default {
-  }
+
+export default {}
 </script>
 
 <style lang="scss" scoped>
-
-  footer {
-    padding: 50px 0 65px;
-    text-align: center;
-  }
+.header{
+  background-color: #fff;
+}
+.header-inner {
+  display: flex;
+  align-items: center;
+}
+.header-title{
+  color: #333;
+}
 </style>
